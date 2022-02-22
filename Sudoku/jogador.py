e06cb1037de6bf9a56616b3dd5015810140952ad
carolinegall <100231106+carolinegall@users.noreply.github.com>
2022-02-22 18:12:55 -0300

    Create jogador.py

---
+++ Sudoku/jogador.py
@@ -0,0 +1,3 @@
+class jogador:
+    def __init__(nome, erros = 0, tempo = 0):   #Ira receber dados do jogador, como tambem a quantidade de erros cometidos e o tempo dele de resolução do jogo 
+        pass 
