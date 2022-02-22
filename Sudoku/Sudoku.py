a281e5843abea8bd9c3929c817abbe0b6b72dc68
carolinegall <100231106+carolinegall@users.noreply.github.com>
2022-02-22 18:08:02 -0300

    Create Sudoku.py

---
+++ Sudoku/Sudoku.py
@@ -0,0 +1,30 @@
+# Propriedades do documento
+
+'''Sudoku é um jogo Sudoku baseado na lógica de colocação de números.''
+__autor__ = 'Caroline de oliveira galla'
+__copyright__ = 'Copyright_2021'
+__créditos__ = __autor__
+__license__ = 'CG'
+__versão__ = '1.0.0'
+__maintainer__ = 'Caroline_Gall' # Responsável por manter o programa funcionando
+__email__ = 'caroline.gall@poli.ufrj.br'
+__status__ = 'Produção'
+
+da importação válida do Sudoku *
+da tela importação *
+da importação do tabuleiro *
+da importação do jogador *
+de importação mecanicas *
+
+class Sudoku(Mecanicas): #Ira receber as mecanicas do jogo
+
+    def __init__(self, jogador = 1): #Recebera o parâmetro de jogadores, sempre 1.
+        passar
+    def solucao(self, solve): #Mostrara a solução caso seja essencial
+        passar
+    def ganhou(self, find_empty): #Ira analisar e apresenta uma mensagem que ganhou quando o jogador completa
+        passar
+
+    def __str__(self, funcao): #Representação em string
+        manual = manual.Tela
+        return (print(f'{funcao} : {manual[funcao]}'))
