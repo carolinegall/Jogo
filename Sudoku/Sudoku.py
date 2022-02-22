83623a0a5a0f6a901756811a6fc15c97a6fb9d27
carolinegall <100231106+carolinegall@users.noreply.github.com>
2022-02-22 20:05:56 -0300

    Update Sudoku.py

--- Sudoku/Sudoku.py
+++ Sudoku/Sudoku.py
@@ -14,8 +14,6 @@ from validSudoku import *
 from tela import *
 from tabuleiro import *
 from jogador import *
-from mecanicas import *
-from numpy as np import *
 class Sudoku(Mecanicas): #Ira receber as mecanicas do jogo
 
     def __init__(self, jogador = 1): #Recebera o parâmetro de jogadores, sempre 1.
